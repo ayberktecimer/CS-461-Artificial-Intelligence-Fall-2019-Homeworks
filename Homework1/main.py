62abe85ac913fd02e8da97a0970b9f0dd586f365
Bengi Dönmez <bengidonmez97@gmail.com>
2019-10-19 16:35:45 +0300

    Comments added

--- Homework1/main.py
+++ Homework1/main.py
@@ -1,34 +1,45 @@
 """
+CS461 Homework1
+Fall 2019
+
 Adahan Yalçınkaya
 Bengi Dönmez
 Emre Sülün
 Eray Şahin
 Kazım Ayberk Tecimer
-
 """
+
 import queue
 
-"""
-This class represents the node in the graph. Each node corresponds to the state of the west coast.
-"""
+
 class State:
+    """
+    This class represents the node in the graph. Each node corresponds to the state of the west coast.
+    """
     total_missionaries = 4
     total_cannibals = 4
-    
 
     def __init__(self, m, c, b, parent, children):
-        self.m = m  # Number of missionaries on the west coast
-        self.c = c  # Number of cannibal on the west coast
-        self.b = b  # Boats position( 1 if its on the west, 0 if its on the east)
+        """
+        Constructor
+        :param m: Number of missionaries on the west coast
+        :param c: Number of cannibal on the west coast
+        :param b: Boats position( 1 if its on the west, 0 if its on the east)
+        :param parent: Previous state
+        :param children: List of next states
+        """
+        self.m = m
+        self.c = c
+        self.b = b
         self.parent = parent
         self.children = children
         self.value = str(self.m) + "M " + str(self.c) + "C " + str(self.b) + "B"
 
-    """ 
-    This function creates possible children for current node.
-    
-    """
     def create_possible_edges(self):
+        """
+        This function creates possible children for current node.
+        :return:
+        """
         possibleChildren = []
         if self.b == 1:
             possibleChildren.append(State(self.m - 2, self.c, 0, self, []))
@@ -47,11 +58,12 @@ class State:
             if child.isStateValid() and child.isLoopFree(self.parent):
                 self.children.append(child)
 
-    """
-    It also checks whether the possible chidren are in safe state (The number missionaries 
-    are greater or equal to number of cannibals in one side ).
-    """
     def isStateValid(self):
+        """
+        It checks whether the state is safe (The number missionaries
+        are greater or equal to number of cannibals in one side).
+        :return: true if state is safe
+        """
         c = self.c
         m = self.m
         b = self.b
@@ -67,10 +79,12 @@ class State:
             return False
         return True
 
-    """
-     It does the loop checking 
-    """
     def isLoopFree(self, parent):
+        """
+        It does the loop checking
+        :param parent:
+        :return:
+        """
         if parent is None:
             return True
         return not (self == parent) and self.isLoopFree(parent.parent)
@@ -79,42 +93,47 @@ class State:
         return self.m == other.m and self.c == other.c and self.b == other.b
 
     def __str__(self):
-        return str(self.m) +"M"+ str(self.c) + 'C' + str(self.b)
+        return str(self.m) + "M" + str(self.c) + 'C' + str(self.b)
 
-    """
-    Checks the current state the goal state.
-    """
     def isStateGoal(self):
+        """
+        Checks if the current state is the goal state.
+        :return:
+        """
         if self.m == 0 and self.c == 0 and self.b == 0:
             return True
         else:
             return False
 
 
-"""
-This function prints the solution path if it exists.
-"""
 def get_solution_path(goal_state):
+    """
+    This function prints the solution path if it exists.
+    :param goal_state:
+    :return:
+    """
     path = []
     current_state = goal_state
     while current_state is not None:
         path.append(current_state)
         current_state = current_state.parent
     path = path[::-1]
-    for i in range(0,len(path)):
+    for i in range(0, len(path)):
         if i == len(path) - 1:
             print(path[i])
         else:
-            print(path[i],end=' -> ')
+            print(path[i], end=' -> ')
 
 
-"""
-This function applies BFS to the states by forming one element queue consisting only root node
-Until first state in the queue is the goal state or the queue becomes empty, it removes the
-first state in the queue and adds the children of the removed state on the rear of the queue.
-If the goal state is found it announce success otherwise it announce failiure.
-"""
 def bfs_tree_search(root):
+    """
+    This function applies BFS to the states by forming one element queue consisting only root node
+    Until first state in the queue is the goal state or the queue becomes empty, it removes the
+    first state in the queue and adds the children of the removed state on the rear of the queue.
+    If the goal state is found it announces success otherwise it announce failure.
+    :param root: starting node of the bfs
+    :return:
+    """
     bfs_queue = queue.Queue()
     bfs_queue.put(root)
 
@@ -130,13 +149,16 @@ def bfs_tree_search(root):
             current_state.create_possible_edges()
             for child in current_state.children:
                 bfs_queue.put(child)
-    
+
     if not isSolutionFound:
         print('No Solution')
 
 
-# Code adapted from https://vallentin.io/2016/11/29/pretty-print-tree 
 def pprint_tree(node, file=None, _prefix="", _last=True):
+    """
+    Pretty print tree
+    Code adapted from https://vallentin.io/2016/11/29/pretty-print-tree
+    """
     print(_prefix, "`- " if _last else "|---- ", node.value, sep="", file=file)
     _prefix += "   " if _last else "|  "
     child_count = len(node.children)
@@ -145,6 +167,7 @@ def pprint_tree(node, file=None, _prefix="", _last=True):
         pprint_tree(child, file, _prefix, _last)
 
 
+# Code starts from here
 initial_state = State(State.total_missionaries, State.total_cannibals, 1, None, [])
 bfs_tree_search(initial_state)
 pprint_tree(initial_state)
