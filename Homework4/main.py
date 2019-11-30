a34b1c74dd7a01d4cde8e6fa7677238f7d516038
Ayberk Tecimer <ayberktecimer@yahoo.com>
2019-11-30 12:44:57 +0200

    to adahan

--- Homework4/main.py
+++ Homework4/main.py
@@ -9,3 +9,27 @@ Eray Şahin 21502758
 Kazım Ayberk Tecimer 21502531
 """
 
+graph = {
+    'Crazy': ['Professors','Hackers'],
+    'Professors': ['Eccentrics', 'Teachers'],
+    'Hackers': ['Eccentrics','Programmers'],
+    'Eccentrics': ['Dwarfs'],
+    'Teachers': ['Dwarfs'],
+    'Programmers': ['Dwarfs'],
+    'Dwarfs': ['Everything'],
+}
+
+def get_reachable_path():
+    return -1
+
+def create_fish_hook_pairs(graph,node):
+    pairs = []
+    for i in range(0,len(graph[node])):
+        
+    pairs.append([node, graph[node][0]])
+    
+
+
+def print_topological_sort(initial_node,graph):
+
+
