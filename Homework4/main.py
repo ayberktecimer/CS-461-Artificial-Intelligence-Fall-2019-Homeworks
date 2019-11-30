090cd0413877e48d6b2e93826dc8e0de0fa19dfa
Adahan Yalçınkaya <adahan.yalcinkaya@gmail.com>
2019-11-30 13:26:26 +0300

    Create fish hook pairs

--- Homework4/main.py
+++ Homework4/main.py
@@ -17,19 +17,41 @@ graph = {
     'Teachers': ['Dwarfs'],
     'Programmers': ['Dwarfs'],
     'Dwarfs': ['Everything'],
+    "Everything": []
 }
 
 def get_reachable_path():
     return -1
 
-def create_fish_hook_pairs(graph,node):
-    pairs = []
-    for i in range(0,len(graph[node])):
-        
-    pairs.append([node, graph[node][0]])
-    
+def create_fish_hook_pairs(node):
+    pairs = {}
+    queue = []
+    visited = set([])
 
+    queue.append(node)
+    while len(queue) != 0:
+        targetNode = queue[0]
+        combinedList = [targetNode] + graph[targetNode]
+        for parent in graph[targetNode]:
+            if parent not in visited:
+                queue.append(parent)
+                visited.add(parent)
+        
+        for i, value in enumerate(combinedList):
+            if i != len(combinedList) - 1:
+                if targetNode in pairs:
+                    pairs[targetNode].append((value, combinedList[i+1]))    
+                else:
+                    pairs[targetNode] = [(value, combinedList[i+1])]
+
+
+        del queue[0]
+    
+    print("Pairs", pairs)
 
 def print_topological_sort(initial_node,graph):
+    pass
+
+create_fish_hook_pairs("Crazy")
 
 
